2eb716d6675ff939c2671cbf6b6a7d28fab766cc
Alexander Kombeiz <alexkombie@gmail.com>
2022-08-02 15:05:27 +0200

    changed cda creation by filling up template

--- src/common.py
+++ src/common.py
@@ -20,8 +20,10 @@
 #
 
 import uuid
-from datetime import datetime
+from datetime import datetime, timedelta
+from random import randint, randrange
 
+from dateutil import parser
 from pytz import timezone
 
 
@@ -34,9 +36,40 @@ class TimeStampHandler:
         d = datetime.now(self.__TIMEZONE)
         return d.strftime(format_date)
 
+    @staticmethod
+    def get_random_date(start='20200101', end='20220101', format_date='%Y%m%d%H%M%S') -> str:
+        start = parser.parse(start)
+        end = parser.parse(end)
+        delta = end - start
+        int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+        random_second = randrange(int_delta)
+        d = start + timedelta(seconds=random_second)
+        return d.strftime(format_date)
 
-class RandomIdGenerator:
+
+class RandomStuffGenerator:
+
+    def __init__(self, pat_id_start=1000, enc_id_start=1000):
+        self.__PATIENT_ID = pat_id_start
+        self.__ENCOUNTER_ID = enc_id_start
 
     @staticmethod
     def get_random_uuid() -> str:
         return str(uuid.uuid4())
+
+    @staticmethod
+    def get_random_postalcode() -> str:
+        start = 10 ** 4
+        end = (10 ** 5) - 1
+        return str(randint(start, end))
+
+    def get_enumerating_patient_id(self) -> str:
+        pat_id = str(self.__PATIENT_ID)
+        pat_id = ''.join(['P', pat_id])
+        self.__PATIENT_ID += 1
+        return pat_id
+
+    def get_enumerating_encounter_id(self) -> str:
+        enc_id = str(self.__ENCOUNTER_ID)
+        self.__ENCOUNTER_ID += 1
+        return enc_id
--- src/header_creator.py
+++ src/header_creator.py
@@ -23,38 +23,56 @@ import random
 
 import lxml.etree as ET
 
-from common import RandomIdGenerator, TimeStampHandler
+from common import RandomStuffGenerator, TimeStampHandler
+from patient_creator import XMLHandler
 
 
 class CDAHeaderCreator:
 
     def __init__(self):
         self.__TIMESTAMP_HANDLER = TimeStampHandler()
-        self.__RANDOM_ID_GENERATOR = RandomIdGenerator()
+        self.__RANDOM_GENERATOR = RandomStuffGenerator()
 
-    def add_cda_header(self, tree: ET.ElementTree) -> ET.Element:
+    def add_cda_header(self, tree: ET.ElementTree) -> ET.ElementTree:
         tree = self.__add_header_meta(tree)
+        tree = self.__add_record_target(tree)
         return tree
 
     def __add_header_meta(self, tree: ET.ElementTree) -> ET.ElementTree:
-        realm_code = ET.Element('realmCode', attrib={'code': 'DE'})
-        id_type = ET.Element('typeId', attrib={'root':      '2.16.840.1.113883.1.3',
-                                               'extension': 'POCD_HD000040'})
-        id_template = ET.Element('templateId', attrib={'root': '1.2.276.0.76.10.1019'})
-        id_doc = ET.Element('id', attrib={'root':      '1.2.276.0.76.3.1.192',
-                                          'extension': self.__RANDOM_ID_GENERATOR.get_random_uuid()})
-        code_doc_type = ET.Element('code', attrib={'code':           '68552-9',
-                                                   'codeSystem':     '2.16.840.1.113883.6.1',
-                                                   'codeSystemName': 'LOINC',
-                                                   'displayName':    'Emergency medicine Note'})
-        title = ET.Element('title')
-        title.text = 'AKTIN CDA Testcase'
-        date_created = ET.Element('effectiveTime', attrib={'value': self.__TIMESTAMP_HANDLER.get_current_date()})
-        code_confidentiality = ET.Element('confidentialityCode', attrib={'code':       'N',
-                                                                         'codeSystem': '2.16.840.1.113883.5.25'})
-        code_language = ET.Element('languageCode', attrib={'code': 'de-DE'})
-        id_set = ET.Element('setId', attrib={'root':      '1.2.276.0.76.4.17.9814184919',
-                                             'extension': self.__RANDOM_ID_GENERATOR.get_random_uuid()})
-        version = ET.Element('version', attrib={'value': '1'})
-        tree.getroot().extend([realm_code, id_type, id_template, id_doc, code_doc_type, title, date_created, code_confidentiality, code_language, id_set, version])
+        root = tree.getroot()
+        elem_id = tree.find('/id', root.nsmap)
+        elem_id.attrib['extension'] = self.__RANDOM_GENERATOR.get_random_uuid()
+        date_created = tree.find('/effectiveTime', root.nsmap)
+        date_created.attrib['value'] = self.__TIMESTAMP_HANDLER.get_random_date(start='19600101')
+        set_id = tree.find('/setId', root.nsmap)
+        set_id.attrib['extension'] = self.__RANDOM_GENERATOR.get_random_uuid()
         return tree
+
+    def __add_record_target(self, tree: ET.ElementTree) -> ET.ElementTree:
+        base_path = '/recordTarget/patientRole'
+        root = tree.getroot()
+        patient_id = tree.find('/'.join([base_path, 'id']), root.nsmap)
+        patient_id.attrib['extension'] = self.__RANDOM_GENERATOR.get_enumerating_patient_id()
+        postal_code = tree.find('/'.join([base_path, 'addr', 'postalCode']), root.nsmap)
+        postal_code.text = self.__RANDOM_GENERATOR.get_random_postalcode()
+        birth_time = tree.find('/'.join([base_path, 'patient', 'birthTime']), root.nsmap)
+        birth_time.attrib['value'] = self.__TIMESTAMP_HANDLER.get_random_date()
+        name, gender_code = self.__get_random_gender_elements()
+        patient_name = tree.find('/'.join([base_path, 'patient', 'name', 'given']), root.nsmap)
+        patient_name.text = name
+        birth_time.addprevious(gender_code)
+        return tree
+
+    @staticmethod
+    def __get_random_gender_elements():
+        gender = random.choices(['M', 'F', 'UN'], weights=[0.45, 0.45, 0.1])[0]
+        if gender == 'UN':
+            gender_code = ET.Element('administrativeGenderCode', attrib={'code':        'UN',
+                                                                         'codeSystem':  '2.16.840.1.113883.5.1',
+                                                                         'displayName': 'Undifferentiated'})
+            patient_name = 'M'
+        else:
+            gender_code = ET.Element('administrativeGenderCode', attrib={'code':       gender,
+                                                                         'codeSystem': '2.16.840.1.113883.5.1'})
+            patient_name = 'Max' if gender == 'M' else 'Maximiliane'
+        return patient_name, gender_code
